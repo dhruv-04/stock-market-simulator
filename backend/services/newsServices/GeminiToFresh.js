9a2d4142771cffba11d6bc17f8d00a71be65e497
Dhruv Kumar <dhruv.kum.04@gmail.com>
2025-03-20 02:07:04 +0530

    news added to freshNews key

--- backend/services/newsServices/GeminiToFresh.js
+++ backend/services/newsServices/GeminiToFresh.js
@@ -5,13 +5,32 @@ const { generateNews } = require('../../utils/stock/fetchGeminiNews');
 const fetchNews = async() => {
     try {
         const news = await generateNews();
-        await insertIntoFreshNews(JSON.stringify(news));
-        console.log('Fetched news:', news);
+        if(news) {
+            await insertIntoFreshNews(JSON.stringify(news));
+            console.log('Fetched news:', news);
+        } else if (news === null) {
+            const failMessage = {
+                news: "The stock market is stable, with no significant changes reported. Investors are monitoring global trends.",
+                sentiment: "Neutral",
+                volatility: "Low",
+                sector: "General Market",
+                company: "Global Index",
+                createdAt: new Date().toISOString()
+            };
+            await insertIntoFreshNews(JSON.stringify(failMessage));
+        }
     } catch (err) {
         console.error('Error fetching news:', err);
         throw err;
     }
 };
+
+// const test = async() => {
+//     await fetchNews();
+//     console.log('Fetched news');
+// };
+
+// test();
 
 module.exports = {
     fetchNews
